e95c6ad4ed6e12f7e620ac2fc31f080f31244861
Edil2222 <zusupovaerkinai@mail.com>
2024-10-28 20:56:41 +0600

    v.2

--- src/script/script.js
+++ src/script/script.js
@@ -12,3 +12,17 @@ function decreaseBalance(amount) {
         alert("Casino Rich: У вас достаточно фишек!");
     }
 }
+
+document.getElementById("openModalBtn").addEventListener("click", function () {
+    document.getElementById("winModal").classList.add("open")
+})
+
+document.getElementById("closeModal").addEventListener("click", function () {
+    document.getElementById("winModal").classList.remove("open")
+})
+
+window.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        document.getElementById("winModal").classList.remove("open")
+    }
+})
