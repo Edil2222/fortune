2b78ec3441a64d33553f32ffa2e61dd32bd04e13
Edil2222 <zusupovaerkinai@mail.com>
2024-10-31 21:03:08 +0600

    Final

--- src/script/script.js
+++ src/script/script.js
@@ -1,29 +1,32 @@
-let chips = 10000;
+let chips = 1000;
 
 function displayChips() {
-    const balanceElement = document.getElementById('chips-total')
-    balanceElement.textContent = chips
+    const balanceElement = document.getElementById('chips-total');
+    balanceElement.textContent = chips;
 }
+
 function decreaseBalance(amount) {
     if (chips - amount >= 0) {
         chips -= amount;
         displayChips();
     } else {
-        alert("Casino Rich: У вас достаточно фишек!");
+        alert("Casino Rich: У вас недостаточно фишек!");
     }
 }
 
+let wheel = document.querySelector('.wheel');
+let spinBtn = document.querySelector('.play');
 
-document.getElementById("openModalBtn").addEventListener("click", function () {
-    document.getElementById("winModal").classList.add("open")
-})
+spinBtn.onclick = function () {
+    if (chips >= 1000) {
+        let rotationValue = Math.ceil(Math.random() * 3600) + 3600;
+        wheel.style.transition = "transform 5s cubic-bezier(0.1, 0.8, 0.5, 1)";
+        wheel.style.transform = `rotate(${rotationValue}deg)`;
 
-document.getElementById("closeModal").addEventListener("click", function () {
-    document.getElementById("winModal").classList.remove("open")
-})
-
-window.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-        document.getElementById("winModal").classList.remove("open")
+        decreaseBalance(1000);
+    } else {
+        alert("Casino Rich: У вас недостаточно фишек для игры!");
     }
-})
+};
+
+displayChips();
